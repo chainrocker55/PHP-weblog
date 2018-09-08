9da772bef61219ae9d6c332d906acd4ad277170b
59160089 <59160089@go.buu.ac.th>
2018-09-08 18:41:54 +0700

    add )

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	  		try{
 				session_start();
 				session_destroy();
-			    $connection= new Mongo(getenv("mongodb://chain:chain555@ds245082.mlab.com:45082/webblog");
+			    $connection= new Mongo(getenv("mongodb://chain:chain555@ds245082.mlab.com:45082/webblog"));
  				 $db= $connection->webblog;
   			 $collection=$db->user;
 				 if((isset($_POST['username'])&&isset($_POST['password']))){
